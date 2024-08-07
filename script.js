ae60fca2e3d9ce8d47ed1cd6c488cfc8d48a6388
MikDra1 <mikolajkuleczka@gmail.com>
2024-08-07 21:05:01 +0200

    Third commit

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const btn = document.querySelector('.btn');
-const advice = document.querySelector('.advice')
-const id = document.querySelector('.id')
+const advice = document.querySelector(".advice");
+const id1 = document.querySelector(".id");
 
-btn.addEventListener("click", async function() {
-    const res = await fetch("https://api.adviceslip.com/advice");
+btn.addEventListener("click", async function () {
+  try {
+    let slip__id = Math.floor(Math.random() * 250 + 1) 
+    console.log(slip__id)
+    const res = await fetch(`https://api.adviceslip.com/advice/${slip__id}`);
     const data = await res.json();
-    // console.log(data)
     advice.textContent = data.slip.advice;
-    id.textContent = data.slip.id;
+    id1.textContent = slip__id;
+  } catch (err) {
+    console.error(err);
+  }
 });
+
