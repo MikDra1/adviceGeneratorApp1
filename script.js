02a096245b5e71b2073969e35f8581ad2b627026
MikDra1 <mikolajkuleczka@gmail.com>
2024-08-07 19:51:41 +0200

    Second commit

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ const btn = document.querySelector('.btn');
 const advice = document.querySelector('.advice')
 const id = document.querySelector('.id')
 
-btn.addEventListener('click', newAdvice)
-
-async function newAdvice() {
+btn.addEventListener("click", async function() {
     const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json()
-    advice.textContent = data.slip.advice
-    id.textContent = data.slip.id
-}
-
-// 
-// newAdvice()
+    const data = await res.json();
+    // console.log(data)
+    advice.textContent = data.slip.advice;
+    id.textContent = data.slip.id;
+});
